569fb8bf1c41f438b0dea0918998f88385f109a8
Jonayet Hossain <jonayet.hossain@adidas.com>
2021-03-09 15:24:32 +0100

    wip

--- deploy/jobs/config.groovy
+++ deploy/jobs/config.groovy
@@ -3,7 +3,7 @@ import groovy.transform.Field
 @Field
 static rootFolderName = 'glass'
 
-
+@Field
 static glassCloneOptions = [
   gitUrl: 'https://github.com/jonayet/jenkins-changset-poc.git',
   gitCredentials: 'github'
